4746a5622d1b2ea9d3f7a9b12ac9e8b71101671b
Catalina Rincón <catainc.cr@gmail.com>
2021-12-05 21:06:21 -0500

    Configure bnv_menu

--- app/src/main/java/com/example/deportestic/view/ui/fragments/LoginFragment.kt
+++ app/src/main/java/com/example/deportestic/view/ui/fragments/LoginFragment.kt
@@ -5,6 +5,8 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Button
+import androidx.navigation.fragment.findNavController
 import com.example.deportestic.R
 
 // TODO: Rename parameter arguments, choose names that match
@@ -32,10 +34,17 @@ class LoginFragment : Fragment() {
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
-        savedInstanceState: Bundle?
-    ): View? {
+        savedInstanceState: Bundle?): View? {
         // Inflate the layout for this fragment
         return inflater.inflate(R.layout.fragment_login, container, false)
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        val button = view.findViewById<Button>(R.id.butonLogin)
+        button?.setOnClickListener {
+            findNavController().navigate(R.id.navHomeFragment, null)
+        }
     }
 
     companion object {
